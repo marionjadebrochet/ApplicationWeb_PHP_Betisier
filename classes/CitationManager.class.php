2f559dc2e92b5c1c503b8e41667276f45cb83f25
Clement Le Caloch <clement.le-caloch@etu.unilim.fr>
2019-12-03 15:23:58 +0100

    Rechercher citation

--- classes/CitationManager.class.php
+++ classes/CitationManager.class.php
@@ -52,5 +52,26 @@ class CitationManager{
       $req->bindValue(':date_depo',$newCitation->getCitDateDep(),PDO::PARAM_STR);
       $req->execute();
   }
+  public function getBonnesCitations($num,$date1,$date2,$basse,$haute)
+  {
+    $listeCitation = array();
+
+    $sql = 'SELECT CONCAT(p.per_nom, p.per_prenom)
+    as cit_nom_enseignant, c.cit_libelle as cit_libelle,
+    c.cit_date as cit_date, AVG(v.vot_valeur) as cit_moyenne FROM CITATION c
+    LEFT JOIN PERSONNE p ON p.per_num = c.per_num
+    LEFT JOIN VOTE v ON c.cit_num = v.cit_num
+    WHERE c.cit_valide = 1 AND c.cit_date_valide is not null AND c.per_num = '.$num.'
+    GROUP BY c.cit_num, p.per_nom, c.cit_libelle, c.cit_date
+    HAVING AVG(v.vot_valeur) > '.$basse.' AND AVG(v.vot_valeur) < '.$haute;
+
+    $requete = $this->db->query($sql);
+    $requete->execute();
+    while ($citation = $requete->fetch(PDO::FETCH_OBJ))
+        $listeCitation[] = new Citation($citation);
+
+    $requete->closeCursor();
+    return $listeCitation;
+  }
 }
 ?>
---
+++ include/pages/rechercherCitation.inc.php
@@ -0,0 +1,54 @@
+//ajouter gestion des dates
+
+<?php
+	$pdo=new Mypdo();
+	$citationManager = new CitationManager($pdo);
+  $personneManager = new PersonneManager($pdo);
+  $personnes=$personneManager->getListSalarie();
+	$citations=$citationManager->getList();
+?>
+
+
+<h1>Rechercher une citation</h1>
+<?php  if(empty($_POST['nom']) && empty($_POST['citation']) && empty($_POST['date'])) {?>
+  <form class="" action="#" method="post">
+  <label>Enseignant : </label>
+  <select name="nom">
+    <?php foreach ($personnes as $personne) { ?>
+      <option value="<?php echo $personne->getPerNum();?>"><?php echo $personne->getPerNom(); ?></option>
+    <?php } ?>
+  </select><br><br>
+  <label for="date1">Compris entre le :</label>
+  <input type="date" name="date1" value="2000-01-01">
+  <label for="date2">et le :</label>
+  <input type="date" name="date2" value="2020-01-01" ><br><br>
+  <label> Note comprise entre : </label>
+  <input type="number" name="basse" value="0" min="0" max="19">
+  <label> et : </label>
+  <input type="number" name="haute" value="20" min="1" max="20">
+  <br><br>
+  <button type="submit" name="button">Rechercher</button>
+  </form>
+
+<?php } else {
+
+  $bonneCitations=$citationManager->getBonnesCitations($_POST['nom'],$_POST['date1'],$_POST['date2'],$_POST['basse'],$_POST['haute']);
+
+  ?>
+  <table>
+    <tr>
+      <th>Nom de l'enseignant</th>
+      <th>Libellé</th>
+      <th>Date</th>
+      <th>Moyenne des note</th>
+    </tr>
+    <?php foreach ($bonneCitations  as $citation) { ?>
+    <tr>
+        <td><?php echo $citation->getCitNomEnseignant(); ?></td>
+        <td><?php echo $citation->getCitLibelle(); ?></td>
+        <td><?php echo $citation->getCitDate(); ?></td>
+        <td><?php echo $citation->getCitMoyenne();?></td>
+    </tr>
+    <?php } ?>
+  </table>
+<?php } ?>
